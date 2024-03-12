b91c661ab2deb203d1d8c42aa0f9f83916f4882f
sootape123 <157672902+sootape123@users.noreply.github.com>
2024-03-13 00:55:26 +0900

    중앙대학교 오픈소스sw와 파이썬프로그래밍 03주차 1주차 과제, 박스의 부피 계산

---
+++ volume_calc.py
@@ -0,0 +1,10 @@
+width = float(input("가로의 길이를 입력하세요"))
+length = float(input("세로의 길이를 입력하세요"))
+height = float(input("높이의 길이를 입력하세요"))
+
+volume = width * length * height
+
+print("가로 :", width)
+print("세로 :", length)
+print("높이 :", height)
+print("박스의 부피는 ",volume,"입니다.")
